e358bdff110c42674d93c295ba3ca51cac7403f3
Willian Herrera <wherrera4@gmail.com>
2025-06-26 03:12:41 -0500

    Creación del componente de calculadora con estructura inicial y uso de Shadow DOM

--- Public/js/calculadora.js
+++ Public/js/calculadora.js
@@ -1,3 +1,62 @@
 class CalculadoraBasica extends HTMLElement {
-    
+    constructor() { 
+        super();
+        
+        // Se crea el Shadow DOM para trabajar con nuestro propio HTML y estilos
+        this.shadow = this.attachShadow({ mode: 'open'});
+
+        // Se agrega el HTML y los estilos de Bootstrap dentro del shadow
+        this.shadow.innerHTML = `
+        <!-- Se importa Bootstrap para estilos -->
+        <link rel="stylesheet" href="/public/lib/bootstrap-5.3.6-dist/css/bootstrap.min.css">
+
+        <!-- Contenedor principal de la calculadora -->
+        <div class="card p-3 mt4">
+            <h4 class="mb-3>Calculadora Básica</h4>
+
+            <!-- Campo para escribir el primer número -->
+            <div class="mb-2">
+                <input type="number" id="numero1" class="form-control" placeholder="Ingrese el primer número">
+            </div>
+
+            <!-- Campo para escribir el segundo número -->
+            <div class="mb-2">
+                <input type="number" id="numero2" class="form-control" placeholder="Ingrese el segundo número">
+            </div>
+
+             <!-- Menú para escoger la operación (suma, resta, etc.) -->
+            <div class="mb-2">
+                <select id="operacion" class="form-select">
+                    <option value="suma">Suma</option>
+                    <option value="resta">Resta</option>
+                    <option value="multiplicacion">Multiplicación</option>
+                    <option value="division">División</option>
+                </select>
+            </div>
+
+            <!-- Botón para hacer el cálculo -->
+            <button id="calcular" class="btn btn-primary w-100">Calcular</button>
+
+            <!-- Aquí se muestra el resultado después del cálculo -->
+            <div id="resultado" class="alert alert-info mt3" role="alert">
+                Resulatdo: <span id="valor">---</span>
+            </div>
+        </div>
+        `;
     }
+
+    // Esto se ejecuta cuando el componente ya está en la página
+    connectedCallback() {
+        // Buscamos el botón y le ponemos un evento para que funcione al hacer clic
+        const btn = this.shadow.querySelector('#calcular');
+        btn.addEventListener('click', () => this.realizarCalculo())
+    }
+
+    // Función para realizar el cálculo
+    realizarCalculo() {
+
+    }
+}
+
+// Registramos nuestro nuevo componente con el nombre que usaremos en HTML
+customElements.define('calculadora-basica', CalculadoraBasica);
